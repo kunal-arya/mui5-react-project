0753432e79744c2b1a588e7b6034f3ef9b74d66d
kunal-arya <85951446+kunal-arya@users.noreply.github.com>
2023-02-10 14:36:44 +0530

    Rightbar Component Complete

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,14 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { ThemeProvider } from '@mui/material';
+import theme from './theme';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
       <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
--- src/theme.jsx
+++ src/theme.jsx
@@ -1,18 +1,27 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: "#ff9800",
-            light: "#ffe0b2"
+    // palette: {
+    //     primary: {
+    //         main: "#ff9800",
+    //         light: "#ffe0b2"
+    //     },
+    //     secondary: {
+    //         main: '#15c630'
+    //     },
+    //     otherColor: {
+    //         main: "#999"
+    //     }
+    // },
+    breakpoints: {
+        values: {
+          xs: 0,
+          sm: 700,
+          md: 900,
+          lg: 1200,
+          xl: 1536,
         },
-        secondary: {
-            main: '#15c630'
-        },
-        otherColor: {
-            main: "#999"
-        }
-    }
+      },
 })
 
 export default theme
